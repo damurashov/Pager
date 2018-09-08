9ad6aa2235804d8223da5b692f7ecdde52fb7a7d
mma28 <m.a.murashova24@yandex.ru>
2018-09-08 19:49:38 +0400

    Chat is done

--- src/main/java/com/murashovs/pajer/controller/GreetingController.java
+++ src/main/java/com/murashovs/pajer/controller/GreetingController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 
@@ -35,6 +36,27 @@ public class GreetingController {
 
     @PostMapping
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message msg = new Message(text, tag);
+
+        messageRepository.save(msg);
+        Iterable<Message> messages = messageRepository.findAll();
+
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String text, Map<String, Object> model){
+        Iterable<Message> messages;
+
+        if (text != null && !text.isEmpty()) {
+            messages = messageRepository.findByTag(text);
+        } else{
+            messages = messageRepository.findAll();
+        }
+
+
+        model.put("messages", messages);
         return "main";
     }
 }
--- src/main/java/com/murashovs/pajer/repository/MessageRepository.java
+++ src/main/java/com/murashovs/pajer/repository/MessageRepository.java
@@ -3,5 +3,10 @@ package com.murashovs.pajer.repository;
 import com.murashovs.pajer.entity.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepository extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
 }
