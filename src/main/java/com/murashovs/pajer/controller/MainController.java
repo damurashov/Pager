79efe2143beb4f577b00e3aedf0f9c8fd510779c
sleeplynerd <dmitry.murzon@gmail.com>
2018-09-14 15:00:10 +0400

    Database User-Message join

--- src/main/java/com/murashovs/pajer/controller/MainController.java
+++ src/main/java/com/murashovs/pajer/controller/MainController.java
@@ -2,8 +2,10 @@ package com.murashovs.pajer.controller;
 
 
 import com.murashovs.pajer.entity.Message;
+import com.murashovs.pajer.entity.User;
 import com.murashovs.pajer.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,9 +35,14 @@ public class MainController {
         return "main";
     }
 
-    @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        Message msg = new Message(text, tag);
+//    @PostMapping("/main")
+    @PostMapping("submit_msg")
+    public String add(@AuthenticationPrincipal User user,
+                      @RequestParam String text,
+                      @RequestParam String tag,
+                      Map<String, Object> model
+    ) {
+        Message msg = new Message(text, tag, user);
 
         messageRepository.save(msg);
         Iterable<Message> messages = messageRepository.findAll();
