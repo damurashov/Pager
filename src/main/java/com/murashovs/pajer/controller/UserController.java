b7fc1e3f49a2eca86e1f182aeeb9a0b5d3051b73
sleeplynerd <dmitry.murzon@gmail.com>
2018-09-15 18:45:15 +0400

    Administration privileges

--- src/main/java/com/murashovs/pajer/controller/UserController.java
+++ src/main/java/com/murashovs/pajer/controller/UserController.java
@@ -16,7 +16,7 @@ import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAuthority('ADMIN')")
+@PreAuthorize("hasAuthority('ADMINISTRATOR')")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
