d30182a8b733448c9fa3d8c12f25e623d3ff0d83
mma28 <m.a.murashova24@yandex.ru>
2018-09-15 18:02:40 +0400

    User list, user editing

--- src/main/java/com/murashovs/pajer/entity/Role.java
+++ src/main/java/com/murashovs/pajer/entity/Role.java
@@ -3,7 +3,7 @@ package com.murashovs.pajer.entity;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER, ADMINISTRATOR;
 
     @Override
     public String getAuthority() {
